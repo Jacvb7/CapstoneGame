fcae8e21d8353dcc09c4bd0bae430666901a61a0
besshagan <haganb@southwestern.edu>
2025-02-13 18:38:17 -0600

    I forgot to save before my last commit.

--- Scenes/Bug/firefly.gd
+++ Scenes/Bug/firefly.gd
@@ -1,11 +1,14 @@
-extends Node2D
+extends Bug
 
+func _ready():
+	super._ready()  # Calls parent _ready()
+	
+	species = "Firefly"
+	size = 2.0
+	color = Color.YELLOW
+	speed = 120.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+	print(species + " is glowing!")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func interact():
+	print(species + " lights up when approached.")
