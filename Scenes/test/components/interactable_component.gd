5546b5e5b1f063a5cb25c94fb3bd75027a9efda3
Seraphina Crayton <craytonj@southwestern.edu>
2025-02-18 17:50:15 -0600

    Collision, Interactable Components

---
+++ Scenes/test/components/interactable_component.gd
@@ -0,0 +1,13 @@
+class_name InteractableComponent 
+extends Area2D
+
+signal interactable_activated
+signal interactable_deactivated
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
