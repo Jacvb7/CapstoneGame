70b790813fc882af50bb607bd0a4b6959b2929b2
ReehaR <raufr@southwestern.edu>
2025-02-28 20:49:13 -0600

    Working on the new settings menu

---
+++ Scenes/user settings screen/options_menu.gd
@@ -0,0 +1,25 @@
+class_name OptionsMenu #create its own class
+extends Control
+
+@onready var exit_button: Button = $MarginContainer/VBoxContainer/Exit_button
+
+#create a signal because this will not be able to go back entirely to the main menu and set everything correctly
+#by itsself
+signal exit_options_menu
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#connecting the exit button on button down to the function _on_exit_button_button_down
+	exit_button.button_down.connect(_on_exit_button_button_down) 
+	#To avoid an error where the button is still clickable even when the options menu is gone
+	set_process(false)	#This code will not run at all unless the process is running or set to true.
+	
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_exit_button_button_down() -> void:
+	exit_options_menu.emit #emit the signal
+	set_process(false)
