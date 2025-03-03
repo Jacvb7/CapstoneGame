83c35ca2c2875af3ccf20486ba104d587ab1d134
Seraphina <craytonj@southwestern.edu>
2025-03-02 20:07:56 -0600

    added byte to to testing player scene

--- Scenes/mainmenu/main_menu.gd
+++ Scenes/mainmenu/main_menu.gd
@@ -24,7 +24,7 @@ func _on_settings_pressed() -> void:
 
 
 func _on_start_game_button_down() -> void:
-	get_tree().change_scene_to_file("res://Scenes/drag_and_drop_card_game/drag_drop_main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/test/test_player_house_tilemap.tscn")
 	
 func _on_exit_game_button_down() -> void:
 	get_tree().change_scene_to_file("res://Scenes/drag_and_drop_card_game/drag_drop_main.tscn")
