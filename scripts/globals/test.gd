214702f5b49feb32c17b14709b076f9e5e903497
besshagan <haganb@southwestern.edu>
2025-03-24 10:09:57 -0500

    testing

--- scripts/globals/test.gd
+++ scripts/globals/test.gd
@@ -5,7 +5,4 @@ extends Node
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#Jacob was here
