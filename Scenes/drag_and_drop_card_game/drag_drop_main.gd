4b451a294efdb08b4ae4e0f640e5df8c8986e824
Jacob Valle <jacob.valle@g.austincc.edu>
2025-04-14 10:56:44 -0500

    Pushing "working" state for the t key.

--- Scenes/drag_and_drop_card_game/drag_drop_main.gd
+++ Scenes/drag_and_drop_card_game/drag_drop_main.gd
@@ -17,14 +17,18 @@ func _process(_delta: float) -> void:
 	on_table_pressed()
 	
 func on_table_pressed() -> void: 
-	if Input.is_action_just_pressed("table") and not GlobalVariables.disable_T and GlobalVariables.minigame_ready == true:
+	if Input.is_action_just_pressed("table") and GlobalVariables.disable_T == false and GlobalVariables.minigame_ready == true and GlobalVariables.finish_mini_game == false:
+		get_tree().change_scene_to_file("res://Levels/Level 1/Level 1.tscn")
+		
+	if Input.is_action_just_pressed("table") and GlobalVariables.finish_mini_game == true:
 		get_tree().change_scene_to_file("res://Levels/Level 1/Level 1.tscn")
 		if GlobalVariables.finish_mini_game == true:
-			GlobalVariables.disable_T = true	
+			GlobalVariables.disable_T = true
+				
 	# Used to make sure that the "t" button press isn't disabled until the game is completed
 	#else:
 	#	get_tree().change_scene_to_file("res://Levels/Level 1/Level 1.tscn")
-		
+	
 #func _on_resolutions_item_selected(index: int) -> void:
 	#match index:
 		#0:
