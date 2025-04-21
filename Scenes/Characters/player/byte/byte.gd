cbc49e8661d421b951488073028e95a4a93a1e05
Seraphina <craytonj@southwestern.edu>
2025-04-21 12:50:30 -0500

    added hud

--- Scenes/Characters/player/byte/byte.gd
+++ Scenes/Characters/player/byte/byte.gd
@@ -4,6 +4,7 @@ extends Player
 @export var animated_sprite_2d: AnimatedSprite2D
 @export var is_byte_visable: bool = GlobalVariables.isByteVisable
 @onready var byte: CharacterBody2D = $"."
+@onready var objectives: Control = $"../CanvasLayer2/Objectives"
 
 
 var speed = 40
@@ -14,6 +15,7 @@ func _ready():
 	SignalBus.show_byte.connect(_show_byte)
 	SignalBus.hide_byte.connect(_hide_byte)
 	byte.hide()
+	objectives.hide()
 
 func _show_byte():
 	byte.show()
